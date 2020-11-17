e80da60c6707d215ca45d51f473fd77c384fd56b
antonzaharia <zaharia_anton@yahoo.com>
2020-11-17 20:16:35 +0000

    deploy done

---
+++ dploy-landpage/src/components/Deploy.js
@@ -0,0 +1,19 @@
+import React from "react";
+import pic2 from "../images/Screenshot_3.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export default function Deploy() {
+    return (
+        <div className="trial">
+        <div className="trial-div">
+          <p className="trial-text">Depoly to your domained, secured cloud</p>
+          <a href="#" className="trial-btn">
+            See How it works &nbsp;
+            <FontAwesomeIcon icon={faArrowRight} />
+          </a>
+        </div>
+        <img src={pic2}/>
+      </div>
+    )
+}
--- dploy-landpage/src/containers/Home.js
+++ dploy-landpage/src/containers/Home.js
@@ -3,6 +3,7 @@ import { Container, Row } from 'react-bootstrap'
 import Header from '../components/Header'
 import Trial from '../components/Trial'
 import Head from '../components/Head'
+import Deploy from '../components/Deploy'
 
 export default class Home extends Component {
     render() {
@@ -12,8 +13,10 @@ export default class Home extends Component {
                   <Header class="header"/>
                 </Row>
                 <Head />
+                <hr />
                 <Trial />
-
+                <hr />
+                <Deploy />
             </Container>
         )
     }
