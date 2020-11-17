7935ab5a863acdd991cf1ce7e9dc51f2e80fcb63
antonzaharia <zaharia_anton@yahoo.com>
2020-11-17 19:48:52 +0000

    head style done

--- dploy-landpage/src/containers/Head.js
+++ dploy-landpage/src/containers/Head.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function Head() {
     return (
         <div className="head">
-            <button className="discover-btn">Discover</button>
+            <button className="discover-btn">Discover &nbsp;<FontAwesomeIcon icon={faArrowRight} /></button>
         </div>
     )
 }
