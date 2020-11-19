dcac660d920ad701faa58c5d0292787cfb5b95e1
antonzaharia <zaharia_anton@yahoo.com>
2020-11-19 19:23:08 +0000

    footer style finished

--- dploy-landpage/src/components/Footer.js
+++ dploy-landpage/src/components/Footer.js
@@ -9,22 +9,22 @@ export default function Footer() {
           <br /><br /><br /><br />
           <ListGroup className="list">
           <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Pricing</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">How it Works</Link></ListGroup.Item>
           </ListGroup>
           <ListGroup className="list">
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Terms of Service</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Privacy</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Contact us</Link></ListGroup.Item>
           </ListGroup>
           <ListGroup className="list">
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Create Account</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">Login</Link></ListGroup.Item>
+          <ListGroup.Item><Link className="footer-link" to="/">For Developers</Link></ListGroup.Item>
           </ListGroup>
           <br /><br /><br /><br />
         <div className="footer-bottom">
-          <hr style={{ borderColor: "white" }} />
+          <hr style={{ borderColor: "#484848" }} />
           <p>D-Ploy &copy; 2020</p>
         </div>
       </Container>
