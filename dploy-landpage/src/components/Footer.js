b9e39163217433aca459a522286b3ec690a07aed
antonzaharia <zaharia_anton@yahoo.com>
2020-11-19 20:01:00 +0000

    icons added to footer

--- dploy-landpage/src/components/Footer.js
+++ dploy-landpage/src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 export default function Footer() {
   return (
@@ -8,19 +10,26 @@ export default function Footer() {
       <Container>
           <br /><br /><br /><br />
           <ListGroup className="list">
-          <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Pricing</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">How it Works</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Marketplace</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Pricing</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">How it Works</Link></ListGroup.Item>
           </ListGroup>
           <ListGroup className="list">
-          <ListGroup.Item><Link className="footer-link" to="/">Terms of Service</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Privacy</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Contact us</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Terms of Service</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Privacy</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Contact us</Link></ListGroup.Item>
           </ListGroup>
           <ListGroup className="list">
-          <ListGroup.Item><Link className="footer-link" to="/">Create Account</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">Login</Link></ListGroup.Item>
-          <ListGroup.Item><Link className="footer-link" to="/">For Developers</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Create Account</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">Login</Link></ListGroup.Item>
+            <ListGroup.Item><Link className="footer-link" to="/">For Developers</Link></ListGroup.Item>
+          </ListGroup>
+          <ListGroup className="list-icons">
+              <ListGroup.Item>
+              <FontAwesomeIcon icon={faFacebookF} />
+              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faLinkedin} />
+              </ListGroup.Item>
           </ListGroup>
           <br /><br /><br /><br />
         <div className="footer-bottom">
