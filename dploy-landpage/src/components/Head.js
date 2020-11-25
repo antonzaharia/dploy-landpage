2a77b5e9792983f22d24ee2179112a2ba41d2063
antonzaharia <zaharia_anton@yahoo.com>
2020-11-25 18:24:21 +0000

    head component finished

--- dploy-landpage/src/components/Head.js
+++ dploy-landpage/src/components/Head.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Head() {
-    return (
-        <div className="head">
-            <button className="discover-btn">Discover &nbsp;<FontAwesomeIcon icon={faArrowRight} /></button>
-        </div>
-    )
+  return (
+    <div className="head">
+      <div>
+        <h1 className="deploy-your-business">
+          Deploy your business in 60 seconds
+        </h1>
+        <p className="getting-into-cloud-h">
+          Getting into cloud has never been easier
+        </p>
+      </div>
+
+      <button className="discover-btn">
+        Discover &nbsp;
+        <FontAwesomeIcon icon={faArrowRight} />
+      </button>
+    </div>
+  );
 }
