757843d8bafae63f9355a8fa1902b01550abe4c9
antonzaharia <zaharia_anton@yahoo.com>
2020-12-01 19:34:32 +0000

    head style finished

--- dploy-landpage/src/components/Head.js
+++ dploy-landpage/src/components/Head.js
@@ -5,9 +5,13 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Head() {
   return (
     <div className="head">
+      <div className="square square-head1"></div>
+      <div className="square square-head2"></div>
+      <div className="square square-head3"></div>
+      <div className="square square-head-white"></div>
       <div>
         <h1 className="deploy-your-business">
-          Deploy your business in 60 seconds
+          Deploy your business&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; in 60 seconds
         </h1>
         <p className="getting-into-cloud-h">
           Getting into cloud has never been easier
