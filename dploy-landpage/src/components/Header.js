84fc20c7458dcc902d2eaa98ab779ca63474339e
antonzaharia <zaharia_anton@yahoo.com>
2020-12-02 21:53:44 +0000

    firebase deploy

--- dploy-landpage/src/components/Header.js
+++ dploy-landpage/src/components/Header.js
@@ -5,7 +5,7 @@ import logo from '../images/dploy-logo.svg'
 export default function Header() {
     return (
         <>
-            <div className="header-item logo"><img src={logo} alt="dploy-logo"/></div>
+            <div className="header-item logo"><img className="logo-img" src={logo} alt="dploy-logo"/></div>
             <div  className="header-item head-links">
                 <Link to="/marketplace">Marketplace</Link>
                 <Link to="/pricing">Pricing</Link>
